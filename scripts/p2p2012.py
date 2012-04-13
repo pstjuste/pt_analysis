b00169acc1af1c7932845da5d4104167860fec1d
Pierre St Juste <ptony82@gmail.com>
2012-04-13 15:11:10 -0400

    Added churn and use iterators

--- scripts/p2p2012.py
+++ scripts/p2p2012.py
@@ -8,6 +8,7 @@ import socialModels as sm
 
 out_file = None
 ids = None
+churn_rate = 0.000000
 
 def add_random_ids(g):
 
@@ -20,9 +21,13 @@ def add_random_ids(g):
 
 def get_random_node(g, node, query):
 
-    nodes = g.neighbors(node)
-    next_hop = random.randint(0, len(nodes)-1)
-    return nodes[next_hop]
+    node_it = g.neighbors_iter(node)
+    next_hop = random.randint(1, g.degree(node))
+
+    for i in range(next_hop): 
+        node = node_it.next()
+
+    return node
 
 
 def get_greedy_node(g, node, query):
@@ -31,9 +36,11 @@ def get_greedy_node(g, node, query):
         node = get_random_node(g, node, query)
 
     old_dist = None
-    nodes = g.neighbors(node)
+    node_it = g.neighbors_iter(node)
+    degree = g.degree(node)
 
-    for n in nodes:
+    for i in range(degree):
+        n = node_it.next()
         dist = math.fabs(query - ids[n])
 
         if old_dist == None or dist < old_dist:
@@ -49,16 +56,22 @@ def get_greedy2_node(g, node, query):
         node = get_random_node(g, node, query)
 
     old_dist = None
-    nodes = g.neighbors(node)
+    node_it = g.neighbors_iter(node)
+    degree = g.degree(node)
 
-    for n in nodes:
+    for i in range(degree):
+        n = node_it.next()
         dist = math.fabs(query - ids[n])
 
         if old_dist == None or dist < old_dist:
             node = n
             old_dist = dist
 
-        for nn in g.neighbors(n):
+        nnode_it = g.neighbors_iter(n)
+        ndegree = g.degree(n)
+
+        for j in range(ndegree):
+            nn = node_it.next()
             dist = math.fabs(query - g[nn][0])
 
             if old_dist == None or dist < old_dist:
@@ -70,42 +83,56 @@ def get_greedy2_node(g, node, query):
 
 def put_random_walk(g, ttl, query, par, next_node_funct):
 
+    global churn_rate
     node = None
     start_node = None
+    node_it = g.nodes_iter()
     ttl = ttl/par
-    rnd_idx = random.randint(0, len(g)-1)
-    start_node = g.nodes()[rnd_idx]
+    rnd_idx = random.randint(1, len(g))
 
-    put_nodes = set()
-    put_nodes.add(start_node)
+    for i in range(rnd_idx):
+        start_node = node_it.next()
+
+    put_nodes = {}
+    put_nodes[start_node] = random.random() > churn_rate
 
     for i in range(par):
         node = start_node
 
-        for j in range(ttl):
+        j = 0
+        while j < ttl:
             node = next_node_funct(g, node, query)
-            put_nodes.add(node)
+            put_nodes[node] = random.random() > churn_rate
+            j += 1
 
     return put_nodes
 
 
 def get_random_walk(g, ttl, query, par, next_node_funct, nodes):
 
+    global drop_rate
     node = None
     start_node = None
+    node_it = g.nodes_iter()
     ttl = ttl/par
-    rnd_idx = random.randint(0, len(g)-1)
-    start_node = g.nodes()[rnd_idx]
+    rnd_idx = random.randint(1, len(g))
+
+    for i in range(rnd_idx):
+        start_node = node_it.next()
 
     count = 0
     for i in range(par):
         node = start_node
 
-        for j in range(ttl):
-            count += 1
+        j = 0
+        while j < ttl:
             node = next_node_funct(g, node, query)
-            if node in nodes:
+
+            if node in nodes and nodes[node] == True:
                 return count
+            elif node not in nodes:
+                j += 1
+                count += 1
 
     return "miss"
 
@@ -141,13 +168,13 @@ def sum_edges(g, hops):
 
 def main():
 
-    msg = "usage: ./p2p2012.py type r|g|g2 ttl par tries"
+    msg = "usage: ./p2p2012.py type r|g|g2 ttl par tries churn_rate"
 
-    if len(sys.argv) < 6:
+    if len(sys.argv) < 7:
         print msg
         sys.exit(1)
 
-    global out_file, bcast
+    global out_file, churn_rate
     out_file = sys.stdout
 
     gtype = sys.argv[1]
@@ -155,6 +182,7 @@ def main():
     ttl = int(sys.argv[3])
     par = int(sys.argv[4])
     tries = int(sys.argv[5])
+    churn_rate = float(sys.argv[6])
 
     if gtype == "a":
         g = nx.barabasi_albert_graph(97134, 3)
@@ -173,7 +201,7 @@ def main():
     elif gtype == "h":
         g = nx.random_regular_graph(20, 905668)
     elif gtype == "i":
-        g = nx.read_edgelist(sys.argv[6])
+        g = nx.read_edgelist(sys.argv[7])
 
     if walk == "r":
         lookup(g, ttl, tries, par, get_random_node)
